b675438c0e1a4c4dc08f6a2198cf3d9918d801f0
aruna <aruna.dissanayake@axiatadigitallabs.com>
2022-02-17 23:34:03 +0530

    express app

--- index.js
+++ index.js
@@ -2,42 +2,41 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
-  // GET route to register the callback URL with Facebook.
-  .get('/webhook', (req, res) => {
-    const VERIFY_TOKEN = "random string"
-    // Parse the query params
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
+express().use(express.static(path.join(__dirname, 'public')))
+express().set('views', path.join(__dirname, 'views'))
+express().set('view engine', 'ejs')
+express().listen(PORT, () => console.log(`Listening on ${PORT}`))
+// GET route to register the callback URL with Facebook.
+express().get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = "random string"
+  // Parse the query params
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
 
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
 
-      // Checks the mode and token sent is correct
-      if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
-        // Responds with the challenge token from the request
-        console.log('WEBHOOK_VERIFIED');
-        res.status(200).send(challenge);
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
 
-      } else {
-        // Responds with '403 Forbidden' if verify tokens do not match
-        res.sendStatus(403);
-      }
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
     }
-  })
+  }
+})
 
-  // POST route to handle webhook calls.
-  .post('/webhook', (req, res) => {
-    try {
-      console.log(req.body);
-    }
-    catch (error) {
-      console.error(error);
-    }
-  })
+// POST route to handle webhook calls.
+express().post('/webhook', (req, res) => {
+  try {
+    console.log(req.body);
+  }
+  catch (error) {
+    console.error(error);
+  }
+})
